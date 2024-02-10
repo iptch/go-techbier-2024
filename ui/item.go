be5933ed5f30fdec2551ea2f43c4b162e40dc9eb
selimkaelin <selimkaelin@outlook.com>
2024-02-10 14:05:21 +0100

    add task 2 solution comments

--- ui/item.go
+++ ui/item.go
@@ -13,12 +13,26 @@ type PokemonItem pokeapi.PokeapiRef[pokeapi.Pokemon]
 // check if type implements interface
 var _ list.Item = (*PokemonItem)(nil)
 
+// ### Task 2 ###
+// Through some research on the Go package index we find out that the
+// list.DefaultItem interface is composed of the list.Item interface
+// and two extra methods Title() and Description(). This is called
+// embedding.
+// This means we need to define the three methods Title(), Description(),
+// and FilterValue(). We do this for a receiver object PokemonItem.
+
 func (i PokemonItem) Title() string {
+	// English Titles are tricky, so let's make sure that we get it right
+	// by using cases.Title()
 	return cases.Title(language.AmericanEnglish).String(i.Name)
 }
 
 func (i PokemonItem) Description() string {
+	// We do an empty implementation for Description, since we only want
+	// to show the Name of the Pokemon in the list view.
 	return ""
 }
 
+// We enable filtering by the item's name. Go can be pretty concise for
+// short methods and functions, as this one-line implementation shows.
 func (i PokemonItem) FilterValue() string { return i.Name }
