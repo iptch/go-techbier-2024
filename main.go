5bedc18b1a8fa8bf8408ecf22cdd341263b8b67e
Zak Cook <zakcook4@gmail.com>
2025-03-27 13:14:03 +0100

    spell pokemon correctly in logs

--- main.go
+++ main.go
@@ -13,21 +13,21 @@ func main() {
 		log.Fatalf("Error getting count: %s\n", err)
 	}
 
-	log.Println("Collecting pokemon...")
+	log.Println("Collecting Pokémon...")
 	results, err := pokeapi.GetAllPokemon()
 	if err != nil {
-		log.Fatalf("Error collecting Pokemon: %s\n", err)
+		log.Fatalf("Error collecting Pokémon: %s\n", err)
 	}
 	for _, pokemonRef := range results {
 		fmt.Println(pokemonRef.Name)
 	}
-	log.Printf("%d/%d Pokemon collected.\n", len(results), count)
+	log.Printf("%d/%d Pokémon collected.\n", len(results), count)
 
 	switch len(results) {
 	case count:
 		log.Println("Good job! You've completed task 1.")
 	case 0:
-		log.Println("No pokemon were collected.")
+		log.Println("No Pokémon were collected.")
 	default:
 		log.Println("Not quite there yet. Check your code :)")
 	}
